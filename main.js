2460d8cbe43e6fb5d7beb6be6ada8f281bfa329d
yassinkokabi4@gmail.com <yassinkokabi4@gmail.com>
2022-10-07 18:49:07 +0100

    comiit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+let edit = document.getElementById("test")
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", (ev)=>{
+    edit.classList.toggle("one");
+   
+    
+});
